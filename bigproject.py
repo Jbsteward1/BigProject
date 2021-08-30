783bf0d571b0b26751ab51e284edb305458f5428
Jeremy <jbsteward1@gmail.com>
2021-08-30 14:47:35 -0400

    start of my resturant project gretting and menu

---
+++ bigproject.py
@@ -0,0 +1,8 @@
+greeting =("Hello welcome to Moes")
+print(greeting)
+
+menu =("Moes burrito $ 8.75\nMoes Taco salad $9.50\nMoes Quesidilla $9.00\nMoes Burrito Bowl $10.00\nMoes Kids Meal $6.50")
+print(menu)
+
+
+ 
