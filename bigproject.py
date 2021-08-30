d49e26fc320c29819e7e60a8dc32549f5cf6483f
Jeremy <jbsteward1@gmail.com>
2021-08-30 15:16:56 -0400

    adding user input with interpolation

--- bigproject.py
+++ bigproject.py
@@ -1,8 +1,20 @@
+Wallet = 75
+
 greeting =("Hello welcome to Moes")
 print(greeting)
 
 menu =("Moes burrito $ 8.75\nMoes Taco salad $9.50\nMoes Quesidilla $9.00\nMoes Burrito Bowl $10.00\nMoes Kids Meal $6.50")
 print(menu)
+
+name = input("print")
+food_option1 = input("Moes burrito") 
+food_option2 = input("Moes taco salad")
+food_option3 = input("Moes Quesidilla")
+food_option4 = input("Moes Burrito Bowl")
+food_option5 = input("Moes Kids meal")
+
+print(f"Hello{name}. Welcome to moes, what can i serve you today? {food_option}.Sure thing no problem.")
+
 
 
  
